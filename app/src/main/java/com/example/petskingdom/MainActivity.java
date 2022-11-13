d4619736d852db3168820af87a9f35ce32450a2b
Andryan <andryancoolz@gmail.cpm>
2022-11-13 11:42:23 +0700

    Improve UI

--- app/src/main/java/com/example/petskingdom/MainActivity.java
+++ app/src/main/java/com/example/petskingdom/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.petskingdom;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -14,10 +16,17 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.petskingdom.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton accountBtn, logoutBtn;
     private ActivityMainBinding binding;
+
+    //firebase auth
+    private FirebaseAuth firebaseAuth;
+
+    //progress dialog
+    private ProgressDialog progressDialog;
 
     @SuppressLint("NonConstantResourceId")
     @Override
@@ -31,10 +40,18 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(navView, navController);
 
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setMessage("Successfully Logout !!!");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        firebaseAuth = FirebaseAuth.getInstance();
         accountBtn = findViewById(R.id.accountButton);
         logoutBtn = findViewById(R.id.logoutButton);
 
         logoutBtn.setOnClickListener(view -> {
+            progressDialog.show();
+            firebaseAuth.signOut();
             Intent loginIntent = new Intent(MainActivity.this, Login.class);
             startActivity(loginIntent);
             finish();
